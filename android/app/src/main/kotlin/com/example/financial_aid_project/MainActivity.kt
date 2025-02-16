4731f7f06ee241c4cb97e8d04e2f565f9f237784
Darren Drummond <DarrenDrummond22@gmail.com>
2025-02-15 21:34:47 -0500

    Base File

---
+++ android/app/src/main/kotlin/com/example/financial_aid_project/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.financial_aid_project
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/firebase_options.dart
@@ -0,0 +1,58 @@
+import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
+import 'package:flutter/foundation.dart'
+    show defaultTargetPlatform, kIsWeb, TargetPlatform;
+
+class DefaultFirebaseOptions {
+  static FirebaseOptions get currentPlatform {
+    if (kIsWeb) {
+      return web;
+    }
+    switch (defaultTargetPlatform) {
+      case TargetPlatform.android:
+        return android;
+      case TargetPlatform.iOS:
+        throw UnsupportedError(
+          'DefaultFirebaseOptions have not been configured for iOS - '
+          'you can reconfigure this by running the FlutterFire CLI again.',
+        );
+      case TargetPlatform.macOS:
+        throw UnsupportedError(
+          'DefaultFirebaseOptions have not been configured for macOS - '
+          'you can reconfigure this by running the FlutterFire CLI again.',
+        );
+      case TargetPlatform.windows:
+        throw UnsupportedError(
+          'DefaultFirebaseOptions have not been configured for Windows - '
+          'you can reconfigure this by running the FlutterFire CLI again.',
+        );
+      case TargetPlatform.linux:
+        throw UnsupportedError(
+          'DefaultFirebaseOptions have not been configured for Linux - '
+          'you can reconfigure this by running the FlutterFire CLI again.',
+        );
+      default:
+        throw UnsupportedError(
+          'DefaultFirebaseOptions are not supported for this platform.',
+        );
+    }
+  }
+
+  static const FirebaseOptions android = FirebaseOptions(
+    apiKey: 'AIzaSyD1xHxkmwYljQ4ANts36Z6Q8d8m9CT8bGM',
+    appId: '1:858689639966:android:ec1c83933789264967400f',
+    messagingSenderId: '858689639966',
+    projectId: 'financial-aid-firebase',
+    storageBucket: 'financial-aid-firebase.firebasestorage.app',
+  );
+
+  // Web configuration
+  static const FirebaseOptions web = FirebaseOptions(
+    apiKey: "AIzaSyDtyW1HBHINno2seYUR7V4D7o8iFJaFT_I",
+    authDomain: "financial-aid-firebase.firebaseapp.com",
+    projectId: "financial-aid-firebase",
+    storageBucket: "financial-aid-firebase.firebasestorage.app",
+    messagingSenderId: "858689639966",
+    appId: "1:858689639966:web:cb981a66a033df5767400f",
+    measurementId: "G-XW2Z4YCLBL",
+  );
+}
