6872918753da996dfea799a5d355852d229482f5
Darren Drummond <DarrenDrummond22@gmail.com>
2025-02-19 22:38:17 -0500

    Firebase_Cli Error Fixed

--- lib/features/authentication/views/home_screen.dart
+++ lib/features/authentication/views/home_screen.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'login_screen.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+  const HomeScreen({super.key});
   @override
   Widget build(BuildContext context) {
     return Scaffold(
